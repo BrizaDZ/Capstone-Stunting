412cf76dd92ffcc390e65eb096f35d3416682a35
LAPTOP-1BU753BU\Lingga <linggaasri16@gmail.com>
2025-02-24 20:34:49 +0700

    relationship

---
+++ app/Http/Controllers/Master/RelationshipController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Master;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Yajra\DataTables\Html\Builder;
+use Yajra\DataTables\Services\DataTable;
+use App\Models\PatientRelationship;
+use DataTables;
+use DB;
+use Auth;
+
+class RelationshipController extends Controller
+{
+    public function index()
+    {
+        return view('admin.master.relationship.index');
+    }
+
+    public function Add()
+    {
+        return view('admin.master.relationship.addedit', ['data' => new PatientRelationship]);
+    }
+
+    public function Edit($id)
+    {
+        return view('admin.master.relationship.addedit', ['data' => PatientRelationship::findOrFail($id)]);
+    }
+
+    public function store(Request $v)
+    {
+        if ($v->id == 0) {
+            $data = new PatientRelationship;
+        } else {
+            $data = PatientRelationship::findOrFail($v->id);
+        }
+
+        $data->name = $v->name; 
+        $data->save();
+
+        return ['success' => true];
+
+    }
+
+
+    public function Ajax(Request $request)
+    {
+        $data = PatientRelationship::select([
+            'RelationshipID',
+            'name',
+
+        ]);
+        $datatables = Datatables::of($data);
+
+        return $datatables->addIndexColumn()->make(true);
+
+    }
+
+    public function search(Request $v)
+    {
+        if (empty($v->q)) {
+            $data = PatientRelationship::select(['RelationshipID', 'name'])->orderBy('name', 'asc')->get();
+        } else {
+            $data = PatientRelationship::where('name', 'like', '%' . $v->q . '%')->orderBy('name', 'asc')->get();
+        }
+
+        return response()->json($data);
+    }
+
+    public function delete($id)
+    {
+        PatientRelationship::find($id)->delete();
+
+        return response()->json(['success' => 'Product deleted successfully.']);
+    }
+}
---
+++ resources/views/admin/master/relationship/addedit.blade.php
@@ -0,0 +1,28 @@
+<form action="/master/relationship/store" method="post">
+    <div class="modal-header">
+        <h4 class="modal-title">
+            @if ($data->RelationshipID != 0)
+                Edit Lokasi Penjemputan
+            @else
+                Tambah Lokasi Penjemputan Baru
+            @endif
+        </h4>
+        <button type="button" class="close text-red" data-dismiss="modal">&times;</button>
+    </div>
+
+    <div class="modal-body ">
+        <input type="hidden" name="id" value="{{ $data->RelationshipID }}" />
+        <div class="mb-3 row">
+            <div class="col-xl-6">
+                <div class="form-group">
+                    <label>Relationship Name</label>
+                    <input type="text" name="name" class="form-control" required autocomplete="off"
+                        value="{{ $data->name }}" />
+                </div>
+            </div>
+        </div>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Tutup</button>
+        <button type="submit" class="btn btn-success">Simpan</button>
+    </div>
+</form>
