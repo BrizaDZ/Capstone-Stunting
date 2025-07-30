592207fc66494ed172c9455cfe59a2511bdebc2f
BrizaDZ <brizadhycorayna@gmail.com>
2025-07-30 10:23:18 +0700

    update

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -51,6 +51,6 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
         Auth::login($user);
 
-        return redirect()->route('dahsboard');
+        return redirect('/');
     }
 }
