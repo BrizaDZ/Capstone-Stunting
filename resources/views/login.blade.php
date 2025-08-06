a6709bc3a5df69d62cd4cb39dae3877db8dbf1e4
BrizaDZ <brizadhycorayna@gmail.com>
2025-08-06 20:32:55 +0700

    update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -70,7 +70,7 @@
 
             <!-- Icon & Title -->
             <div class="col-12 col-md-6 order-1 order-md-2 text-center text-md-end mb-0  mb-md-0">
-                <h1 class="mt-4 mb-3 fs-1 fw-medium me-md-4">Stunt<span class="text-primary">AID</span></h1>
+                <h1 class="mt-4 mb-3 fs-1 fw-medium me-md-4">Stunt<span class="text-primary">AIDS</span></h1>
                 <p class="fs-5 lh-base me-md-4 text-body-secondary">
                     Welcome to StuntAIDS!<br>
                     where we make a healthy future.
